c87b9d0ae83e8ae4a2398c4315f72e2043e4aff7
palaksaxena92 <94341176+palaksaxena92@users.noreply.github.com>
2022-07-19 09:05:53 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function Home() {
+  const handleClick=()=>{
+    localStorage.clear();
+    window.location.reload();
+    }
   return (
     <>
       <h1>Login Successfully</h1>
+    <button onclick={handleClick}>Logout</button>
     </>
   );
 }
